b3cb106d363bc3cbcd9a888e10cb0be56aac211e
syyad <syyadkhan@yahoo.com>
2020-11-07 17:35:11 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        React Starter App with Material-UI + Navbar
+      </header>
+    </div>
+  );
+}
+
+export default App;
